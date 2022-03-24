4e3e5fb733de2e6659616e5a7e6457955030b38b
JuanJoseEscobar <cheche.juan@hotmail.com>
2022-03-23 23:14:22 -0500

    copia

---
+++ Practices/02.Clean-Code/PrintMultiples/PrintMultiples/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace PrintMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMultiples(100);
+        }
+
+        private static void PrintMultiples(int number)
+        {
+            Console.Write(getNumbersUntil(number));
+        }
+
+        private static string getNumbersUntil(int number)
+        {
+            string ListNumbers = "";
+            for (int i = 1; i <= number; i++)
+            {
+                //ListNumbers += isMultiple(i , 3) ? isMultiple(i , 5) ? (M-3-5)+"" : (M-3)+"" : isMultiple(i , 5) ? (M-5)+"" : (i)+"";
+
+                if(isMultiple(i , 3))
+                {
+
+                    if(isMultiple(i , 5))
+                    {
+                        ListNumbers += "M-3-5";
+                    }else{
+                        ListNumbers += "M-3";
+                    }
+
+                }else if(isMultiple(i , 5))
+                {
+
+                    ListNumbers += "M-5";
+
+                }else{
+
+                    ListNumbers += (i)+"";
+
+                }
+
+                ListNumbers += i == number ? ".": ",";
+            }
+
+            return ListNumbers;
+        }
+
+        private static bool isMultiple(int number, int multiple)
+        {
+            return (number % multiple == 0);
+        }
+
+        /************** version 0.1 ************
+        /* private static void PrintMultiples(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if(i % 3 == 0){
+                    if(i % 5 == 0){
+                        Console.Write($"c3:{i-3-5},");
+                    }
+                    else
+                    {
+                        Console.Write($"c1:{i-3},");
+                    }
+                }
+                else if(i % 5 == 0){
+                    Console.Write($"c2:{i-5},");
+                }
+                else{
+                    Console.Write($"c0:{i},");
+                }
+            }
+        } */
+        /************** version 0.1 ************/
+    }
+}
---
+++ Practices/02.Clean-Code/PrintPrimes/PrintPrimes/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace PrintPrimes
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      PrintPrimes(13);
+      // 2,3,5,7,11,13
+      PrintPrimes(10);
+      // 2,3,5,7
+      PrintPrimes(0);
+      // Error: Invalid Number
+      PrintPrimes(1);
+      // Error: 1 Is not Prime
+
+    }
+
+    private static void PrintPrimes(int number)
+    {
+      
+      Console.WriteLine(getPrimesFrom(number));
+      
+    }
+
+    private static string getPrimesFrom(int number){
+      string primesCatch = "";
+
+      if (number == 1)
+      {
+        return ("Error: 1 Is not Prime");
+      }
+      if (number <= 0)
+      {
+        return ("Error: Invalid Number");
+      }
+
+      for (int currentNumber = 1; currentNumber <= number; currentNumber++)
+      {
+        if(validatePrime(currentNumber))
+        {
+          primesCatch += currentNumber.ToString()+",";
+        }
+      }
+      return primesCatch;
+    }
+
+    private static bool validatePrime(int currentNumber){
+        
+        for (int i = 2; i < currentNumber; i++)
+        {
+          if(IsDivider(currentNumber,i))
+          {
+            return false;
+          }
+        }
+
+      return true;
+    }
+
+    private static bool IsDivider(int number, int divider){
+
+      return number % divider == 0;
+    }
+
+    //************* old code version ************* si es mi codigoooo
+    /* private static void PrintPrimes(int number)
+    {
+      try
+      {
+        Console.WriteLine(getPrimesFrom(number));
+      }
+      catch (NotSupportedException e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+
+    private static string getPrimesFrom(int number){
+      string primesCatch = "";
+      const int DividersOfPrime = 2;
+
+      if (number == 1)
+      {
+        throw new NotSupportedException("Error: 1 Is not Prime");
+      }
+      if (number <= 0)
+      {
+        throw new NotSupportedException("Error: Invalid Number");
+      }
+      for (int currentNumber = 1; currentNumber <= number; currentNumber++)
+      {
+        int contDivisor = 0;
+        for (int i = 1; i <= currentNumber; i++)
+        {
+          if(currentNumber % i == 0)
+          {
+            contDivisor += 1;
+          }
+          if(contDivisor > DividersOfPrime)
+          {
+            break;
+          }
+        }
+        if(contDivisor == DividersOfPrime)
+        {
+          primesCatch += currentNumber.ToString()+",";
+        }
+      }
+      return primesCatch;
+    } */
+
+    
+  }
+}
+
